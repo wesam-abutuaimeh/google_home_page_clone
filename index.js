0e77d3959120d457e70c1b64c4d7f23cf4c12b35
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-06-04 19:36:31 +0300

    git add update colors as assignment requirement with multi theme

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ window.addEventListener("click", (e) => {
     appsMenu.classList.remove("active");
   }
 });
+
+function toggleTheme() {
+  const htmlTag = document.getElementsByTagName("html")[0];
+  if (htmlTag.hasAttribute("data-theme")) {
+    htmlTag.removeAttribute("data-theme");
+  } else {
+    htmlTag.setAttribute("data-theme", "light");
+    document.querySelector(".google-logo").style.cssText = "background-image: url('https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'); background-repeat: no-repeat; background-color:#fff;";
+  }
+  document.querySelector(".google-logo").style.cssText = "background-image: url('/imgs/google_logo.svg'); background-repeat: no-repeat; background-color:#fff;";  
+}
